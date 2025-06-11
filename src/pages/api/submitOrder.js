48aa832fa6d7e1530adc6c672e8d31547efa235d
Kshitij8713345 <Kshitijsonibca@gmail.com>
2025-06-11 17:46:28 +0530

    Initial commit

--- src/pages/api/submitOrder.js
+++ src/pages/api/submitOrder.js
@@ -1,11 +1,11 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const orderData = req.body;
+// export default function handler(req, res) {
+//   if (req.method === 'POST') {
+//     const orderData = req.body;
 
-    console.log('Received order:', orderData); // You’ll see this in Vercel logs
+//     console.log('Received order:', orderData); // You’ll see this in Vercel logs
 
-    res.status(200).json({ message: 'Order received successfully' });
-  } else {
-    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
-  }
-}
+//     res.status(200).json({ message: 'Order received successfully' });
+//   } else {
+//     res.status(405).json({ message: `Method ${req.method} Not Allowed` });
+//   }
+// }
